ae170ffd72c2fe71144cd353ec95f3e7948efaed
wangkawang <lijie187888@163.com>
2025-12-04 20:25:38 +0800

    Delete export.py

--- export.py
+++
@@ -1,7 +0,0 @@
-import warnings
-warnings.filterwarnings('ignore')
-from ultralytics import RTDETR
-
-if __name__ == '__main__':
-    model = RTDETR('weight-path')
-    model.export(format='onnx', simplify=True)
